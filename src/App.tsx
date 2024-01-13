8218e2abb37e8f7940823275e038bf1aa893f4e2
Jesusborjas006 <jesusbor006@gmail.com>
2024-01-13 16:30:04 -0600

    Successfully fetch from json server and store data to menu state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+
+const url = "http://localhost:9000";
+
 function App() {
+  const [menu, setMenu] = useState([]);
+  console.log(menu);
+
+  useEffect(() => {
+    const fetchMenu = async () => {
+      const response = await fetch(`${url}/menu`);
+      const data = await response.json();
+      setMenu(data);
+    };
+    fetchMenu();
+  }, []);
+
   return (
     <>
       <h1>Burger Shack</h1>
