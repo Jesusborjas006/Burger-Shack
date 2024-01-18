0eb3a2dc02ecbacf5fbb6550933141610fe6da10
Jesusborjas006 <jesusbor006@gmail.com>
2024-01-17 19:19:04 -0600

    Add message when there's no orders in cart

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -27,22 +27,28 @@ const CartPage = ({ cartItems }: CartPageProps) => {
       </h2>
       <div className="max-w-[1640px] mx-auto px-6 pb-10 flex flex-col lg:flex-row">
         <div className="flex flex-col gap-7 justify-evenly shadow-md border lg:mr-10 lg:w-[70%] py-8 rounded-2xl">
-          {cartItems.map((item) => (
-            <div
-              key={item.id}
-              className="flex items-center justify-between px-4 md:px-8 lg:px-12"
-            >
-              <div className="flex items-center">
-                <img
-                  src={item.img}
-                  alt={item.name}
-                  className="w-[50px] h-[50px] md:w-[100px] md:h-[100px] rounded-full object-cover mr-2 md:mr-5"
-                />
-                <p className="font-semibold md:text-lg ">{item.name}</p>
-              </div>
-              <p>${item.price}</p>
-            </div>
-          ))}
+          {cartItems.length === 0 ? (
+            <p className="text-center">Add an item to view order</p>
+          ) : (
+            <>
+              {cartItems.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex items-center justify-between px-4 md:px-8 lg:px-12"
+                >
+                  <div className="flex items-center">
+                    <img
+                      src={item.img}
+                      alt={item.name}
+                      className="w-[50px] h-[50px] md:w-[100px] md:h-[100px] rounded-full object-cover mr-2 md:mr-5"
+                    />
+                    <p className="font-semibold md:text-lg ">{item.name}</p>
+                  </div>
+                  <p>${item.price}</p>
+                </div>
+              ))}
+            </>
+          )}
         </div>
         <div className="flex flex-col-reverse lg:flex-col mt-10 lg:mt-0 gap-10 ">
           <div className="shadow-md min-w-[300px] border flex justify-around items-center h-[70px] font-bold text-lg w-[30%] rounded-2xl">
