13fa447e63d50d79995b395c9f5ce6857faf962e
Jesusborjas006 <jesusbor006@gmail.com>
2024-01-19 15:46:31 -0600

    Fix styling for responsive design

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -32,13 +32,13 @@ const MenuItem = ({ item, handleCartItems }: MenuItemProps) => {
           ${item.price} | {item.calories} cals
         </p>
         <div className="flex justify-evenly mt-8">
-          <Link to={`/details/${item.id}`} className="w-[50%]">
+          <Link to={`/details/${item.id}`} className="">
             <button className="border-2 border-orange-500 text-orange-500 font-bold rounded-full px-4 py-2">
               Details
             </button>
           </Link>
           <select
-            className="border"
+            className="border rounded-full pl-4 w-[18%]"
             value={quantity}
             onChange={(e) => setQuantity(Number(e.target.value))}
           >
@@ -47,7 +47,7 @@ const MenuItem = ({ item, handleCartItems }: MenuItemProps) => {
             <option value={3}>3</option>
           </select>
           <button
-            className="bg-orange-500 text-white font-bold rounded-full w-[50%]"
+            className="bg-orange-500 text-white font-bold rounded-full w-[40%]"
             onClick={() =>
               handleCartItems({
                 id: Date.now(),
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -35,7 +35,7 @@ const CartPage = ({ cartItems, removeCartItem }: CartPageProps) => {
               {cartItems.map((item) => (
                 <div
                   key={item.id}
-                  className="flex items-center justify-between gap-x-2 px-4 md:px-8 lg:px-12"
+                  className="flex items-center justify-between px-4 md:px-8 lg:px-12"
                 >
                   <div className="flex items-center">
                     <img
@@ -43,15 +43,19 @@ const CartPage = ({ cartItems, removeCartItem }: CartPageProps) => {
                       alt={item.name}
                       className="w-[50px] h-[50px] md:w-[100px] md:h-[100px] rounded-full object-cover mr-2 md:mr-5"
                     />
-                    <p className="font-semibold md:text-lg ">{item.name}</p>
+                    <p className="font-semibold text-sm md:text-lg ">
+                      {item.name}
+                    </p>
                   </div>
-                  <div className="flex items-center gap-x-8">
-                    <p>Quantity: {item.quantity}</p>
-                    <p className="font-bold">
+                  <div className="flex items-center gap-x-3 md:gap-x-8">
+                    <p className="font-semibold text-sm sm:text-base">
+                      Qt: {item.quantity}
+                    </p>
+                    <p className="font-bold text-sm sm:text-base">
                       ${Number(item.price * item.quantity).toFixed(2)}
                     </p>
                     <button
-                      className="bg-red-500 text-white w-[30px] py-1 rounded-full font-bold hover:bg-red-800"
+                      className="bg-red-500 text-white w-[25px] sm:w-[30px] sm:py-1 rounded-full font-bold hover:bg-red-800"
                       onClick={() => removeCartItem(item.id)}
                     >
                       X
@@ -63,7 +67,7 @@ const CartPage = ({ cartItems, removeCartItem }: CartPageProps) => {
           )}
         </div>
         <div className="flex flex-col-reverse lg:flex-col mt-10 lg:mt-0 gap-10 ">
-          <div className="shadow-md min-w-[300px] border flex justify-around items-center h-[70px] font-bold text-lg w-[30%] rounded-2xl">
+          <div className="shadow-md min-w-[300px] border flex justify-around items-center h-[70px] font-bold md:text-lg w-[30%] rounded-2xl">
             <h4>Subtotal:</h4>
             <p>${getTotalCost(cartItems)}</p>
           </div>
