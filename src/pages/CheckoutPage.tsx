35702cd8dfdd960eaba41bf58f8585a0a03400c9
Jesusborjas006 <jesusbor006@gmail.com>
2024-01-25 16:50:04 -0600

    Add discount promo code functionality

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -8,6 +8,8 @@ type CheckoutPageProps = {
   setCartItems: React.Dispatch<React.SetStateAction<CartItemsType[] | []>>;
 };
 
+const thePromoCode = "burger";
+
 const CheckoutPage = ({ cartItems, setCartItems }: CheckoutPageProps) => {
   const getSubTotalCost = (arr: CartItemsType[]) => {
     const total = arr.reduce((acc, current) => {
@@ -21,6 +23,7 @@ const CheckoutPage = ({ cartItems, setCartItems }: CheckoutPageProps) => {
     lastName: "",
     phoneNumber: "",
   });
+  const [promoCodeInput, setPromoCodeInput] = useState("");
   const [modalIsActive, setModalIsActive] = useState(false);
 
   const handleFormChange = (e: { target: { name: string; value: string } }) => {
@@ -38,96 +41,112 @@ const CheckoutPage = ({ cartItems, setCartItems }: CheckoutPageProps) => {
   const subTotal = Number(getSubTotalCost(cartItems));
   const tax = subTotal * 0.06;
   const total = subTotal + tax;
+  const discount = total * 0.2;
+  const newDiscountedTotal = total - discount;
 
   return (
     <section>
       <Navbar children={undefined} />
-      <div className="border min-w-[340px] max-w-[450px] mx-auto px-6 pb-10 my-14 rounded-xl shadow-md">
-        <form className="space-y-8">
-          <h2 className="text-3xl font-bold mt-4">Contact Info</h2>
-          <div>
-            <label htmlFor="email">Email Address</label>
-            <br />
-            <input
-              className="border-b w-full outline-none"
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleFormChange}
-              required
-            />
-          </div>
-          <div className="flex justify-between">
+      <div className="px-4">
+        <div className="border min-w-[340px] max-w-[450px] mx-auto px-6 pb-10 my-14 rounded-xl shadow-md">
+          <form className="space-y-8">
+            <h2 className="text-3xl font-bold mt-4">Contact Info</h2>
             <div>
-              <label>First Name</label>
+              <label htmlFor="email">Email Address</label>
               <br />
               <input
-                className="border-b outline-none"
-                type="text"
-                name="firstName"
+                className="border-b w-full outline-none"
+                type="email"
+                id="email"
+                name="email"
+                onChange={handleFormChange}
+                required
+              />
+            </div>
+            <div className="flex flex-col sm:flex-row justify-between space-y-8 sm:space-y-0">
+              <div>
+                <label>First Name</label>
+                <br />
+                <input
+                  className="border-b outline-none w-full"
+                  type="text"
+                  name="firstName"
+                  onChange={handleFormChange}
+                />
+              </div>
+              <div>
+                <label>Last Name</label>
+                <br />
+                <input
+                  className="border-b outline-none w-full"
+                  type="text"
+                  name="lastName"
+                  onChange={handleFormChange}
+                />
+              </div>
+            </div>
+            <div>
+              <label>Phone Number</label>
+              <br />
+              <input
+                className="border-b w-full outline-none"
+                type="tel"
+                name="phoneNumber"
                 onChange={handleFormChange}
               />
             </div>
             <div>
-              <label>Last Name</label>
-              <br />
+              <label>Have a promo code?</label>
               <input
-                className="border-b outline-none"
+                className="border-b w-full outline-none py-2"
                 type="text"
-                name="lastName"
-                onChange={handleFormChange}
+                placeholder="Promo code"
+                onChange={(e) => setPromoCodeInput(e.target.value)}
               />
             </div>
-          </div>
-          <div>
-            <label>Phone Number</label>
-            <br />
-            <input
-              className="border-b w-full outline-none"
-              type="tel"
-              name="phoneNumber"
-              onChange={handleFormChange}
-            />
-          </div>
-          <div>
-            <label>Have a promo code?</label>
-            <input
-              className="border-b w-full outline-none py-2"
-              type="text"
-              placeholder="Promo code"
-            />
-          </div>
-          <div className="flex-col space-y-1 my-8">
-            <p className="flex justify-between">
-              Subtotal: <span>${subTotal}</span>
-            </p>
-            <p className="flex justify-between">
-              Taxes: <span>${tax.toFixed(2)}</span>
-            </p>
-            <p className="flex justify-between font-bold">
-              Total: <span>${total.toFixed(2)}</span>
-            </p>
-          </div>
-          <button
-            className={`text-white w-full rounded-full py-3 font-semibold ${
-              !form.email ||
-              !form.firstName ||
-              !form.lastName ||
-              !form.phoneNumber
-                ? "cursor-not-allowed bg-orange-200"
-                : "bg-orange-500"
-            } `}
-            onClick={handleFormSubmit}
-            disabled={
-              !form.email ||
-              !form.firstName ||
-              !form.lastName ||
-              !form.phoneNumber
-            }
-          >
-            Submit Order
-          </button>
-        </form>
+            <div className="flex-col space-y-1 my-8">
+              <p className="flex justify-between">
+                Subtotal: <span>${subTotal}</span>
+              </p>
+              <p className="flex justify-between">
+                Taxes: <span>${tax.toFixed(2)}</span>
+              </p>
+              {}
+              {promoCodeInput === thePromoCode && (
+                <p className="flex justify-end">20% discount</p>
+              )}
+
+              <p className="flex justify-between font-bold">
+                Total:
+                <span>
+                  $
+                  {promoCodeInput === thePromoCode
+                    ? newDiscountedTotal.toFixed(2)
+                    : total.toFixed(2)}
+                </span>
+              </p>
+            </div>
+            <button
+              className={`text-white w-full rounded-full py-3 font-semibold ${
+                !form.email ||
+                !form.firstName ||
+                !form.lastName ||
+                !form.phoneNumber
+                  ? "cursor-not-allowed bg-orange-200"
+                  : "bg-orange-500"
+              } `}
+              onClick={handleFormSubmit}
+              disabled={
+                !form.email ||
+                !form.firstName ||
+                !form.lastName ||
+                !form.phoneNumber
+              }
+            >
+              Submit Order
+            </button>
+          </form>
+        </div>
       </div>
       {modalIsActive && <OrderConfirmedModal />}
     </section>
