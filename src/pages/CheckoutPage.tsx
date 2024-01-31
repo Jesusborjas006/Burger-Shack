a12deb3fd26a7507d18ab926b8270b641e409925
Jesusborjas006 <jesusbor006@gmail.com>
2024-01-30 18:14:08 -0600

    Add link to checkout page

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -6,6 +6,7 @@ import PromoCodeInput from "../components/PromoCodeInput";
 import CheckoutFormCost from "../components/CheckoutFormCost";
 import ContactInfoInputs from "../components/ContactInfoInputs";
 import SubmitOrderBtn from "../components/SubmitOrderBtn";
+import { Link } from "react-router-dom";
 
 type CheckoutPageProps = {
   cartItems: CartItemsType[];
@@ -35,10 +36,16 @@ const CheckoutPage = ({ cartItems, setCartItems }: CheckoutPageProps) => {
   };
 
   return (
-    <section>
+    <section className="">
       <Navbar children={undefined} />
-      <div className="px-4">
+      <div className="px-4 relative">
         <div className="border min-w-[340px] max-w-[450px] mx-auto px-6 pb-10 my-14 rounded-xl shadow-md">
+          <Link
+            to="/cart"
+            className="text-3xl text-white bg-black hover:text-black hover:bg-white duration-300 absolute rounded-full w-[70px] h-[70px] flex justify-center items-center top-[-10px] left-8 cursor-pointer"
+          >
+            &larr;
+          </Link>
           <form className="space-y-8">
             <h2 className="text-3xl font-bold mt-4">Contact Info</h2>
             <ContactInfoInputs handleFormChange={handleFormChange} />
