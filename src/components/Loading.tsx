468ea870bf756881fab63dc53738487522d6c796
Jesusborjas006 <jesusbor006@gmail.com>
2024-02-15 15:45:55 -0600

    Add loading spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,16 @@
 const Loading = () => {
   return (
     <>
-      <p className="flex justify-center w-full h-svh items-center text-lg font-semibold">
-        Loading...
-      </p>
+      <div className="flex justify-center items-center h-svh">
+        <div
+          className="inline-block h-12 w-12 animate-spin rounded-full border-5 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite] "
+          role="status"
+        >
+          <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+            Loading...
+          </span>
+        </div>
+      </div>
     </>
   );
 };
